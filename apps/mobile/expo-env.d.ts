da18e9119cb7af2698cecdec840c61775f937d91
hunter <hunter.hao.t@gmail.com>
2026-07-21 15:32:14 +0800

    add app patch

--- apps/mobile/expo-env.d.ts
+++ apps/mobile/expo-env.d.ts
@@ -1,4 +1,3 @@
 /// <reference types="expo/types" />
 
-// NOTE: This file should not be edited and should be committed with the rest of
-// your project. It is generated/maintained by Expo.
+// NOTE: This file should not be edited and should be in your git ignore
